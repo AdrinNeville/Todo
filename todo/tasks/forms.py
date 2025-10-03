5e0d78b4f563d258c104f4b26933d46448162f90
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-10-03 10:47:10 +0530

    first commit

---
+++ todo/tasks/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Todo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['title', 'description', 'completed', 'priority', 'due_date']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
