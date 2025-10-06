5ca82b8781f973498dec5466682e81cac0cbbb5b
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-10-06 13:07:48 +0530

    docker to render

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)k5s4wpk2!&2+ndd!q_tveb1&76rc!$kw!^i*ad0ae6_r^98dg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG', default=0))
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
