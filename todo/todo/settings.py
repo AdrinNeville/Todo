57dbad91539421ee9bfc058fcb44d5601f80f1de
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-10-07 16:48:47 +0530

    test

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)k5s4wpk2!&2+ndd!q_tveb1&76rc!$kw!^i*ad0ae6_r^98dg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ['todo-kljg.onrender.com']
 
 # Application definition
 
